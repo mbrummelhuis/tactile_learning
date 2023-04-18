707ff3910e3ca77073f3fc8c74ecb0d5682abfeb
Nathan Lepora <n.lepora@bristol.ac.uk>
2023-04-18 21:45:43 +0100

    integrated mdn models

--- tactile_learning/supervised/image_generator.py
+++ tactile_learning/supervised/image_generator.py
@@ -164,7 +164,7 @@ def demo_image_generation(
 
         for i in range(learning_params['batch_size']):
             for key, item in labels.items():
-                print(key.split('_')[0], ': ', item[i])
+                print(key, ': ', item[i])
             print('')
 
             # convert image to opencv format, not pytorch
---
+++ tactile_learning/supervised/train_model.py
@@ -0,0 +1,65 @@
+from tactile_learning.supervised.simple_train_model import simple_train_model
+from tactile_learning.supervised.train_model_w_metrics import train_model_w_metrics
+from tactile_learning.supervised.train_mdn_model import train_mdn_model
+
+
+def train_model(
+    prediction_mode,
+    model,
+    label_encoder,
+    train_generator,
+    val_generator,
+    learning_params,
+    save_dir,
+    error_plotter=None,
+    calculate_train_metrics=False,
+    device='cpu'
+):
+
+    if 'MDNHead' in str(type(model)):
+        val_loss, train_time = train_mdn_model(
+            prediction_mode,
+            model,
+            label_encoder,
+            train_generator,
+            val_generator,
+            learning_params,
+            save_dir,
+            device=device
+        )
+
+        if error_plotter:
+            print('Error plotter not implemented in MDN Head')
+
+        if calculate_train_metrics:
+            print('Train metrics not implemented in MDN Head')
+
+    else:
+
+        if error_plotter or calculate_train_metrics:
+            val_loss, train_time = train_model_w_metrics(
+                prediction_mode,
+                model,
+                label_encoder,
+                train_generator,
+                val_generator,
+                learning_params,
+                save_dir,
+                error_plotter,
+                calculate_train_metrics,
+                device=device
+            )
+
+        else:
+            val_loss, train_time = simple_train_model(
+                prediction_mode,
+                model,
+                label_encoder,
+                train_generator,
+                val_generator,
+                learning_params,
+                save_dir,
+                device=device
+            )
+
+    return val_loss, train_time 
